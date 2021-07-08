c923ea20264f5b578d02a6ee542fadab27608638
root <omsatam1005@gmail.com>
2021-07-08 12:41:05 +0530

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 //app config
 const app = express()
-const PORT =  process.env.port || 8000;
+const PORT =  process.env.PORT || 8000;
 
 // app middlewares
 app.use(express.json())
